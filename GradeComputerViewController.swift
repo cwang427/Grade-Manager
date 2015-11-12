d1f1dc1c9857b85a13f9300e866e6222cc90bf8d
Cassidy Wang <cjwang14@gmail.com>
2015-11-11 23:40:05 -0500

    Keyboard hide and Login placeholder text
    Keyboard now hides when the view is tapped. Login fields contain
    placeholder text instead of regular title text.

--- GradeComputerViewController.swift
+++ GradeComputerViewController.swift
@@ -12,10 +12,20 @@ class GradeComputerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
 
+    @IBAction func login() {
+        print("logging in")
+    }
 }
