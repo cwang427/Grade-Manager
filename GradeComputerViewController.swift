9846644c4bac7bcca76203b01566f792200b0134
Cassidy Wang <cjwang14@gmail.com>
2015-11-13 23:19:01 -0500

    String security, autocorrect, and keyboard obstruction
    Password string is now securely displayed and username autocorrect is
    disabled. Began writing functionality for movement of view if text
    fields are obstructed by keyboard (not functional at this point).

--- GradeComputerViewController.swift
+++ GradeComputerViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 import Foundation
 
-class GradeComputerViewController: UIViewController {
+class GradeComputerViewController: UIViewController, UITextFieldDelegate {
     
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var windowView: UIView!
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -20,17 +22,63 @@ class GradeComputerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Manage autologin information
-        let userData = NSUserDefaults.standardUserDefaults()
-        if let savedUsername = userData.stringForKey(""),
-            let savedPassword = userData.stringForKey("") {
-                saveCredentials(username: savedUsername, password: savedPassword)
-        }
-        
-        //Dismiss keyboard if view is tapped
+        //Calls function to dismiss keyboard if view is tapped
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
         view.addGestureRecognizer(tap)
     }
+    
+    //FUNCTIONALITY BLOCK: Move text field if obstructed by keyboard
+    var activeField: UITextField?
+    func registerForKeyboardNotifications() {
+        //Add notifications on keyboard showing
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWasShown:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillBeHidden:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func deregisterFromKeyboardNotifications() {
+        //Remove notifications on keyboard showing
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func keyboardWasShown(notification: NSNotification) {
+        //Calculate keyboard size by Apple's suggestions
+        self.scrollView.scrollEnabled = true
+        var info: NSDictionary = notification.userInfo!
+        var keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue().size
+        var contentInsets: UIEdgeInsets = UIEdgeInsetsMake(0.0, 0.0, keyboardSize!.height, 0.0)
+        
+        self.scrollView.contentInset = contentInsets
+        self.scrollView.scrollIndicatorInsets = contentInsets
+        
+        var aRect: CGRect = self.view.frame
+        aRect.size.height -= keyboardSize!.height
+        if let activeFieldPresent = activeField {
+            if (!CGRectContainsPoint(aRect, activeField!.frame.origin)) {
+                self.scrollView.scrollRectToVisible(activeField!.frame, animated: true)
+            }
+        }
+    }
+    
+    func keyboardWillBeHidden(notification: NSNotification) {
+        //Restore original positions when keyboard disappears
+        var info: Dictionary = notification.userInfo!
+        var keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue().size
+        var contentInsets: UIEdgeInsets = UIEdgeInsetsMake(0.0, 0.0, -keyboardSize!.height, 0.0)
+        self.scrollView.contentInset = contentInsets
+        self.scrollView.scrollIndicatorInsets = contentInsets
+        self.view.endEditing(true)
+        self.scrollView.scrollEnabled = false
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        activeField = textField
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        activeField = nil
+    }
+    //END FUNCTIONALITY BLOCK
     
     //Closes keyboard
     func dismissKeyboard() {
