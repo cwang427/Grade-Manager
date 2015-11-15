f00dbf7f4b00807e19d1177c5bcaa31586947959
Cassidy Wang <cjwang14@gmail.com>
2015-11-14 23:17:48 -0500

    Removed scroll view and constraints
    Attempt to adjust view when text field is obstructed by keyboard
    failed; starting over with alternate method

--- GradeComputerViewController.swift
+++ GradeComputerViewController.swift
@@ -26,59 +26,15 @@ class GradeComputerViewController: UIViewController, UITextFieldDelegate {
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
         view.addGestureRecognizer(tap)
     }
+    //////////////////////////////////////////////////////////////////
+    //FUNCTIONALITY BLOCK: Move text field if obstructed by keyboard//
+    //////////////////////////////////////////////////////////////////
     
-    //FUNCTIONALITY BLOCK: Move text field if obstructed by keyboard
-    var activeField: UITextField?
-    func registerForKeyboardNotifications() {
-        //Add notifications on keyboard showing
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWasShown:", name: UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillBeHidden:", name: UIKeyboardWillHideNotification, object: nil)
-    }
     
-    func deregisterFromKeyboardNotifications() {
-        //Remove notifications on keyboard showing
-        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
-    }
     
-    func keyboardWasShown(notification: NSNotification) {
-        //Calculate keyboard size by Apple's suggestions
-        self.scrollView.scrollEnabled = true
-        var info: NSDictionary = notification.userInfo!
-        var keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue().size
-        var contentInsets: UIEdgeInsets = UIEdgeInsetsMake(0.0, 0.0, keyboardSize!.height, 0.0)
-        
-        self.scrollView.contentInset = contentInsets
-        self.scrollView.scrollIndicatorInsets = contentInsets
-        
-        var aRect: CGRect = self.view.frame
-        aRect.size.height -= keyboardSize!.height
-        if let activeFieldPresent = activeField {
-            if (!CGRectContainsPoint(aRect, activeField!.frame.origin)) {
-                self.scrollView.scrollRectToVisible(activeField!.frame, animated: true)
-            }
-        }
-    }
-    
-    func keyboardWillBeHidden(notification: NSNotification) {
-        //Restore original positions when keyboard disappears
-        var info: Dictionary = notification.userInfo!
-        var keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue().size
-        var contentInsets: UIEdgeInsets = UIEdgeInsetsMake(0.0, 0.0, -keyboardSize!.height, 0.0)
-        self.scrollView.contentInset = contentInsets
-        self.scrollView.scrollIndicatorInsets = contentInsets
-        self.view.endEditing(true)
-        self.scrollView.scrollEnabled = false
-    }
-    
-    func textFieldDidBeginEditing(textField: UITextField) {
-        activeField = textField
-    }
-    
-    func textFieldDidEndEditing(textField: UITextField) {
-        activeField = nil
-    }
-    //END FUNCTIONALITY BLOCK
+    ///////////////////////////
+    //END FUNCTIONALITY BLOCK//
+    ///////////////////////////
     
     //Closes keyboard
     func dismissKeyboard() {
